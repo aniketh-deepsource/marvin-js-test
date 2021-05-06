796b8b62835f546c31dc4f584d64d49213cbf57d
aniketh-deepsource <76031695+aniketh-deepsource@users.noreply.github.com>
2021-05-06 16:48:03 +0530

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,1 +1,29 @@
 console.log("foo")
+
+
+
+const express = require('express')
+const helmet = require('helmet')
+const child_process = require('child_process')
+
+let app = express()
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      // other directives
+      frameAncestors: ["'none'"] // frameAncestors  is set to none
+    }
+  })
+)
+
+
+var path = 'user input'
+child_process.exec('ls -l ' + path, function (err, data) {
+  console.log(data)
+})
+
+
+Buffer([1, 2, 3])
+new Buffer([1, 2, 3])
+Buffer.allocUnsafe(5)
